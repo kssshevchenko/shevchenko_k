6c382e470c21df2ef2fa87b2852db765e535576b
Kateryna Shevchenko <sidelnikks2@gmail.com>
2026-07-14 17:10:44 +0300

    Update DB

--- shevchenko_k/settings.py
+++ shevchenko_k/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,13 +80,7 @@ WSGI_APPLICATION = 'shevchenko_k.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'shevchenko_k',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    'default': {dj_database_url.config(default=os.getenv("DATABASE_URL"))
     }
 }
 
