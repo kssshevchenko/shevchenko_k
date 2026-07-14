0d031a99c8fabdc855b267761bc99d7793d808c0
Kateryna Shevchenko <sidelnikks2@gmail.com>
2026-07-14 16:21:13 +0300

    Add ALLOWED_HOSTS

--- shevchenko_k/settings.py
+++ shevchenko_k/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-r4w-x1+eh^diqtn)48&^6#4ob5)q2db_$o)g)bot(*zprj_*7k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shevchenko-k.onrender.com']
 
 
 # Application definition
