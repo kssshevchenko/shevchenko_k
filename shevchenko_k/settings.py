e9c4700cb950039b3c02fa144cd82ca5b838c66e
Kateryna Shevchenko <162917589+kssshevchenko@users.noreply.github.com>
2026-07-23 11:54:20 +0300

    Fix database configuration syntax in settings.py

--- shevchenko_k/settings.py
+++ shevchenko_k/settings.py
@@ -80,8 +80,7 @@ WSGI_APPLICATION = 'shevchenko_k.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {dj_database_url.config(default=os.getenv("DATABASE_URL"))
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
